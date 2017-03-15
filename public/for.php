44b22f7980e4310800cbe6c747da1765d0266514
Todd Hendricks <hendricks.ta@gmail.com>
2017-03-15 10:45:05 -0500

    first draft of for exercise

---
+++ public/for.php
@@ -0,0 +1,15 @@
+<?php
+
+fwrite(STDOUT, 'Input a number');
+
+$x = fgets(STDIN);
+$y = fgets(STDIN);
+
+// x is starting number; y is ending number
+
+for ($x = 0, $x =< $y, $y === 100) {
+	$x++;
+}
+
+
+?>
