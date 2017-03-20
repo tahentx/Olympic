3857257e8d9fd37b00e9591b81bc08f61e256b2b
Todd Hendricks <hendricks.ta@gmail.com>
2017-03-20 13:37:46 -0500

    rough draft solving question 3

--- public/search-arrays.php
+++ public/search-arrays.php
@@ -4,13 +4,28 @@ $names = ['Tina', 'Dana', 'Mike', 'Amy', 'Adam'];
 
 $compare = ['Tina', 'Dean', 'Mel', 'Amy', 'Michael'];
 
-$query = 'Tina';
+$query = 'Bob';
 
-$result = array_search($query, $names, $strict = true);
+$namesResult = array_search($query, $names, $strict = true);
 
-if ($result !== false) {
-	echo $names[$result] . PHP_EOL;
+$compareResult = array_search($query, $compare, $strict = true);
+
+// if ($result !== false) {
+// 	echo $names[$namesResult] . PHP_EOL;
+// } else {
+// 	echo "Your input is not in our database" . PHP_EOL;
+// }
+
+function namesinCommon($namesResult,$compareResult) {
+	foreach ($namesResult as $name) {
+		if (array_search($name, $compareResult)){
+	echo $name;
+	} else {
+		echo "It's not here" . PHP_EOL;	
+	}
+}
 }
 
+namesinCommon($names, $compare);
 
 ?>
